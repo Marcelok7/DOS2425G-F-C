c0af33f4aad9b5d10abbcfbd275c845b3f8bce56
marcomachado93 <153778703+marcomachado93@users.noreply.github.com>
2024-10-18 22:20:54 +0100

    task #1 completa

---
+++ Task_Management_System/TMS/Controller/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TMS.Models;
+
+namespace TMS.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> users = new List<User>
+    {
+        new User
+        {
+            Id = 1,
+            Nome = "Joaquim Lano",
+            Password= "banana",
+            
+        }
+        };
+
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+    }
+
+}
