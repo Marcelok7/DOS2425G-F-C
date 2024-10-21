24031c0233f1929a4f92e6490f8642164eeb4fe5
Marcelo Ferreira <1231546@isep.ipp.pt>
2024-10-21 20:14:53 +0100

    feat/#2 Project Model and Controller created

---
+++ Task_Management_System/TMS/Controller/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TMS.Models;
+
+namespace TMS.Controller
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private static readonly List<ProjectClass> projects = new List<ProjectClass>
+        {
+        new ProjectClass(1, "Kinhas", "Futebol"),
+
+        new ProjectClass(2, "Moove", "Futebol"),
+
+        new ProjectClass(3, "4linhas", "Futebol"),
+        };
+
+        [HttpGet]
+        public IActionResult GetAllProjects()
+        {
+            return Ok(projects);
+        }
+    }
+}
+
