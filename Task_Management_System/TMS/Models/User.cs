e8369339d7382c96dc5727d83adce3418a99b76c
Marco Machado <1231547@isep.ipp.pt>
2024-11-08 20:34:36 +0000

    Feat/#10 Um comentário está associado a um User

--- Task_Management_System/TMS/Models/User.cs
+++ Task_Management_System/TMS/Models/User.cs
@@ -8,6 +8,7 @@
         public string Email { get; set; }
         public string FullName { get; set; }
         public string UserRole { get; set; }
+        public List<Task> Tasks { get; set; } = new List<Task>(); // Lista 
 
     }
 }
