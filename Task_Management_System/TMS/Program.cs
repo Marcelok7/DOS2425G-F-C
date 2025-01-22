f1157815b2d0dc8a25a17a069107a24987115ee9
Pedro Gonçalves <1231551@isep.ipp.pt>
2025-01-22 23:59:52 +0000

    P8 - Working

--- Task_Management_System/TMS/Program.cs
+++ Task_Management_System/TMS/Program.cs
@@ -12,14 +12,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configura o pipeline de requisição HTTP.
-if (app.Environment.IsDevelopment())
+/*/if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NomeDoSeuProjeto v1"));
-}
+}*/
 
-//app.UseSwagger();
-//app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NomeDoSeuProjeto v1"));
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NomeDoSeuProjeto v1"));
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
